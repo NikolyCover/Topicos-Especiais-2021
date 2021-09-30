cc767643ceaea367802ffb644f56e82b3dda898b
Nikoly Cover Pereira <nikoly_cover@outlook.com>
2021-09-30 00:26:25 -0300

    starting activity

---
+++ Atividade/ex1.py
@@ -0,0 +1,27 @@
+import sys
+
+def media(numbers):
+    sum = 0
+
+    for number in numbers:
+        sum += number
+    
+    media = sum / len(numbers)
+    return media
+
+if sys.version_info.major == 2:
+    name = raw_input('Atleta: ')
+elif sys.version_info.major == 3:
+    name = input('Atleta: ')
+
+jumps = []
+jumps.append(input('\nPrimeiro Salto: '))
+jumps.append(input('Segundo Salto: '))
+jumps.append(input('Terceiro Salto: '))
+jumps.append(input('Quarto Salto: '))
+jumps.append(input('Quinto Salto: '))
+
+print('\nResultado final:')
+print('Atleta: ' + name)
+print('Saltos: {} - {} - {} - {} - {}'.format(jumps[0], jumps[1], jumps[2], jumps[3], jumps[4]))
+print(media(jumps) + 'm')
---
+++ Intro/input.py
@@ -0,0 +1,7 @@
+num1 = input('Entre com o 1 valor: ')
+
+num2 = input('Entre com o 2 valor: ')
+
+soma = num1 + num2
+
+print "A soma e",soma
