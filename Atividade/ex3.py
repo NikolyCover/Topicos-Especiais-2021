b3b9c102691de8506c857ece363e15e69e62893e
Nikoly Cover Pereira <nikoly_cover@outlook.com>
2021-10-05 21:39:20 -0300

    add ex3 and ex4

---
+++ Atividade/ex3.py
@@ -0,0 +1,7 @@
+name = raw_input('Digite seu nome: ')
+
+name2 = ''
+
+for letter in name.strip():
+    name2 = name2 + letter
+    print(name2)
---
+++ Atividade/ex4.py
@@ -0,0 +1,8 @@
+def sum(num1, num2, num3):
+    return num1 + num2 + num3
+
+num1 = input('Digite um numero: ')
+num2 = input('Digite outro numero: ')
+num3 = input('Digite mais um numero: ')
+
+print('A soma entre os numeros fornecidor e: {}'.format(sum(num1, num2, num3)))
